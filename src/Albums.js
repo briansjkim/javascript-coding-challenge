ccdf7d9c80d22d5279cdbb542aa897eed66f11b2
Brian Kim <briaansjkim@gmail.com>
2020-06-01 16:40:03 -0400

    working on pagination

--- src/Albums.js
+++ src/Albums.js
@@ -5,6 +5,8 @@ import './App.css';
 const Albums = ({ album }) => {
   const [photos, setPhotos] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [picsPerPage, setPicsPerPage] = useState(18);
   const albumId = album.userId;
 
   useEffect(() => {
@@ -23,9 +25,13 @@ const Albums = ({ album }) => {
     fetchedPhotos();
   }, [albumId]);
 
+  const idxOfLastPic = currentPage * picsPerPage;
+  const idxOfFirstPic = idxOfLastPic - picsPerPage;
+  const currentPics = photos.slice(idxOfFirstPic, idxOfLastPic);
+
   return (
     <div className="Album-photos">
-      <Photos photos={photos} album={album} loading={loading} />
+      <Photos photos={currentPics} album={album} loading={loading} />
     </div>
   )
 }
